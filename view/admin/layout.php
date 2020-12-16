c8c2cbfab715397b84d8e2e00429c8eddffd5803
Nolovenodie <nolovenodie@outlook.com>
2020-12-16 16:11:49 +0800

    添加随机背景，增加遮罩模糊

--- view/admin/layout.php
+++ view/admin/layout.php
@@ -7,10 +7,22 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/css/mdui.min.css" integrity="sha256-lCFxSSYsY5OMx6y8gp8/j6NVngvBh3ulMtrf4SX5Z5A=" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/js/mdui.min.js" integrity="sha256-dZxrLDxoyEQADIAGrWhPtWqjDFvZZBigzArprSzkKgI=" crossorigin="anonymous"></script>
 	<script>$ = mdui.JQ;</script>
+	<link rel="icon" href="https://everking.xyz/wp-content/uploads/2020/03/cropped-0f8d375fc1d3270ea9f776710e01baeb-192x192.png" sizes="192x192" />
 </head>
 <body class="mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-layout-dark mdui-theme-primary-orange">
-<header class="mdui-appbar mdui-appbar-fixed">
-  <div class="mdui-toolbar mdui-color-theme">
+<div style="
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-image: url(https://www.rrll.cc/tuceng/ecy.php);
+	background-size: cover;
+	background-repeat-x: no-repeat;
+	opacity: .2;
+"></div>
+<header class="mdui-appbar mdui-appbar-fixed mdui-color-theme mdui-appbar-inset" style="background-color: rgba(33,33,33,.7) !important;backdrop-filter: blur(20px)">
+  <div class="mdui-toolbar mdui-color-theme" style="background-color: initial !important;">
     <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-drawer="{target: '#main-drawer', swipe: true}"><i class="mdui-icon material-icons">menu</i></span>
     <a href="./" target="_blank" class="mdui-typo-headline mdui-hidden-xs">首页</a>
     <div class="mdui-toolbar-spacer"></div>
@@ -64,7 +76,7 @@
 	<?php view::section('content');?>
 </div>
 <script>
-	$("a[href='<?php echo'?'.(route::get_uri());?>']").addClass("mdui-text-color-blue");
+	$("a[href='<?php echo'?'.(route::get_uri());?>']").addClass("mdui-text-color-drak");
 </script>
 </body>
 
